319001466f5ec9cdf545a4c84e8b0bae1348c858
4KRO5 <4kro54k@gmail.com>
2023-09-20 11:57:47 -0700

    Inicio de sesión

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,61 @@
+<template>
+  <div class="container">
+    <div class="language-select">
+      <label for="language"> {{ language.selectLanguage }} </label>
+      <select id="language" v-model="currentLanguage" @change="changeLanguage(currentLanguage)">
+        <option value="en">English</option>
+        <option value="es">Español</option>
+      </select>
+    </div>
+
+    <fieldset>
+      <legend>{{ language.login }}</legend>
+      <input type="email" v-model="email" :placeholder="language.email">
+      <input type="password" v-model="password" :placeholder="language.password">
+      <button @click="login">{{ language.loginButton }}</button>
+      <p v-if="error" class="error-message">{{ language.errorMessage }}</p>
+    </fieldset>
+  </div>
+</template>
+
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
-const people = ref([])
+const email = ref('')
+const password = ref('')
+const error = ref(false)
 
-const firstName = ref('')
-const lastName = ref('')
+const currentLanguage = ref('en')
 
-const saveData = () => {
-  if (firstName.value && lastName.value) {
-    const person = {
-      firstName: firstName.value,
-      lastName: lastName.value
+import languageData from './language.json'
+
+const changeLanguage = (language) => {
+  currentLanguage.value = language
+}
+
+const language = computed(() => {
+  return languageData[currentLanguage.value]
+})
+
+const login = async () => {
+  try {
+    const response = await fetch('src/users.json')
+    const userData = await response.json()
+
+    const foundUser = userData.find(user => {
+      return email.value === user.email && password.value === user.password
+    })
+
+    if (foundUser) {
+      alert(`${language.value.welcome} ${foundUser.name}`);
+      localStorage.setItem('user_data', JSON.stringify(foundUser))
+      error.value = false
+    } else {
+      error.value = true
     }
-    people.value.push(person)
-    firstName.value = ''
-    lastName.value = ''
+  } catch (e) {
+    console.error(`${language.value.error}`, e);
+    error.value = true
   }
 }
 </script>
-
-<template>
-  <div class="container">
-    <fieldset>
-      <legend>Nombre(s):</legend>
-      <input type="text" v-model="firstName" placeholder="Nombres">
-      <input type="text" v-model="lastName" placeholder="Apellidos">
-      <button @click="saveData">Save Data</button>
-    </fieldset>
-
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Lastname</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="person in people" :key="person.firstName + person.lastName">
-          <td>{{ person.firstName }}</td>
-          <td>{{ person.lastName }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</template>
